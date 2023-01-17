90c533659b98493c61d9e8ba4ce3685526f97f1a
Himanku Gogoi <himankugogoi5@gmail.com>
2023-01-17 19:20:25 +0530

    boiler plate added

---
+++ cw-project/src/Components/Admin/data.js
@@ -0,0 +1,24 @@
+export const iphone = [
+    {
+        id: 1,
+        name: "Pear iPad Air",
+        price: 80000,
+        description: "lorem ipsum text",
+        images: [
+            {
+                id: 1,
+                url: "url"
+            },
+            {
+                id: 2,
+                url: "url"
+            }
+        ]
+    },
+]
+
+export const mac = [
+    {
+
+    },
+]
