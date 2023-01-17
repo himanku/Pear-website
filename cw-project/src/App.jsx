e9c1d5a5dc38e5e4d9c2e5f20a95cce65c5d9529
Himanku Gogoi <himankugogoi5@gmail.com>
2023-01-18 00:23:12 +0530

    test files day-2

--- cw-project/src/App.jsx
+++ cw-project/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import AdminSideBar from "../src/Components/Admin/AdminSideBar"
+import SidebarContent from './Components/Admin/SidebarContent'
 
 function App() {
 
   return (
     <div className="App">
     {/* {Comment the h2 tag and add your desired components while development} */}
-      <h2>Add your page/componets in App.jsx during the development and revert back to it's previous stage before pushing daily to avoid conflicts</h2>
+      {/* <h2>Add your page/componets in App.jsx during the development and revert back to it's previous stage before pushing daily to avoid conflicts</h2> */}
+      {/* <AdminSideBar/> */}
+      <SidebarContent/>
     </div>
   )
 }
