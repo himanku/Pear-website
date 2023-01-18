d384883e40124878f27d9da206527d2fa1bdf932
abhishek1337chatterjee <abhishek1337chatterjee@gmail.com>
2023-01-18 21:51:47 +0530

    updated

--- cw-project/src/Components/InputControl/InputControl.jsx
+++ cw-project/src/Components/InputControl/InputControl.jsx
@@ -4,8 +4,6 @@ export const InputControl = (props) => {
   return (
     <div className={styles.container}>
         {props.label && <label>{props.label}</label>}
-
-        
         <input  {...props} />
     </div>
   )
