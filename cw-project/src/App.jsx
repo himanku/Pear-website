5f281015812bc501635a260a3fcc1d7975ade1e5
sujjwal21 <sujjwal85@gmail.com>
2023-01-18 21:28:49 +0530

    day_1

--- cw-project/src/App.jsx
+++ cw-project/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import AdminSideBar from "../src/Components/Admin/AdminSideBar"
 import SidebarContent from './Components/Admin/SidebarContent'
+import Mac from './Pages/Mac'
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
     {/* {Comment the h2 tag and add your desired components while development} */}
       {/* <h2>Add your page/componets in App.jsx during the development and revert back to it's previous stage before pushing daily to avoid conflicts</h2> */}
       {/* <AdminSideBar/> */}
-      <SidebarContent/>
+      {/* <SidebarContent/> */}
+      <Mac/>
     </div>
   )
 }
