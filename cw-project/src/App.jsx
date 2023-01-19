30ebc29e1f756fb11d15d67c42fc6b4d3fe332c8
Sameerpadhy50 <sameerpadhy50@gmail.com>
2023-01-20 02:54:15 +0530

    day-3 added

--- cw-project/src/App.jsx
+++ cw-project/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Ipad from './Components/iPad/Ipad'
 import AllRoutes from './Pages/AllRoutes'
 function App() {
  return (
  
     <div>
       <AllRoutes/>
+      <Ipad/>
       </div>
   )
 }
