a111e5a2bc15a5b3a938f00b6d52d0489a47fdcb
Vaibhav <103978586+vaibhav15git@users.noreply.github.com>
2023-01-21 22:43:45 +0530

    ok

--- cw-project/src/App.jsx
+++ cw-project/src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
-import AllRoutes from './Pages/AllRoutes'
+import "./App.css";
+import AllRoutes from "./Pages/AllRoutes";
+import HomePage from "./Components/HomePage/HomePage";
+import Swiperrr from "./Components/HomePage/Swiperrr";
+import Footer from "./Components/Footer/Footer";
+import HeroSection from "./Components/Navbar/HeroSection";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
- return (
+  return (
     <div>
-      <AllRoutes/>
+      <AllRoutes />
+      <Navbar />
+
+      <HeroSection />
+      <HomePage />
+      <Swiperrr />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
